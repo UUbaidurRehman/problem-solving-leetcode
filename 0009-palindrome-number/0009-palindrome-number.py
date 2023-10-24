9e7c6ed78dd1bd604fd64b5653b8e65e1ec53356
UBAID-Urrehman0022 <119315741+UBAID-Urrehman0022@users.noreply.github.com>
2023-10-24 22:20:08 +0500

    Time: 49 ms (92.37%), Space: 16.1 MB (80.57%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,10 +1,12 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         x = str(x)
-        right = len(x)-1
-        for char in x:
-            if char == x[right]:
-                right -=1
-            else:
-                return False
-        return True
+        return x == x[::-1]
+        # right = len(x)-1
+
+        # for char in x:
+        #     if char == x[right]:
+        #         right -=1
+        #     else:
+        #         return False
+        # return True
