7668f13cfe1793c0cb13fbf4aceae6ba0baaafe1
UBAID-Urrehman0022 <119315741+UBAID-Urrehman0022@users.noreply.github.com>
2023-10-26 07:31:57 +0500

    Time: 581 ms (82.59%), Space: 29.3 MB (20.52%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -3,11 +3,10 @@ class Solution:
         l, r = 0 , len(height)-1
         max_area = 0
         while(l<r):
-            
-            area = (r-l) * (min(height[r],height[l]))
-            max_area = max(max_area, area)
+            max_area = max(max_area, (r-l) * (min(height[r],height[l])))
             if(height[l] < height[r]):
                 l += 1
             else:
                 r -=1  
         return max_area
+        # calculate area on each set of pointers save max_area
