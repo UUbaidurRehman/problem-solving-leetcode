4220bd7bb10be560d26451b535df29ebfb143cb7
UBAID-Urrehman0022 <119315741+UBAID-Urrehman0022@users.noreply.github.com>
2023-10-28 23:17:48 +0500

    Time: 40 ms (54.71%), Space: 16.4 MB (36.18%) - LeetHub

---
+++ 1274-number-of-days-between-two-dates/1274-number-of-days-between-two-dates.py
@@ -0,0 +1,24 @@
+class Solution:
+    def daysBetweenDates(self, date1: str, date2: str) -> int:
+        def calc_Days(date):
+            stnd_year = 1900
+            days = 0
+            year, month, day = map(int,date.split('-'))
+            for y in range(stnd_year, year):
+                days +=  365
+                if ( y%4 == 0):
+                    if y%100 != 0 or (y%100==0 and y%400==0):
+                        days +=1
+            for m in range(1, month):
+                if m in [1,3,5,7,8,10,12]:
+                    days += 31
+                elif m == 2:
+                    days += 28 
+                    if (year%4 ==0 ):
+                        if year%100 != 0 or ( year%100==0 and year%400==0):
+                            days +=1
+                else: #m in [4,6,9,11]
+                    days +=30
+            return days +day
+        return abs(calc_Days(date1)-calc_Days(date2))
+
