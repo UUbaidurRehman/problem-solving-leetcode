a98cb4e7255615bbfc576c59231262f56226841c
UBAID-Urrehman0022 <119315741+UBAID-Urrehman0022@users.noreply.github.com>
2024-02-09 15:23:03 +0500

    Time: 113 ms (98.65%), Space: 18 MB (79.3%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,13 +3,11 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        i = 0
-        length = len(nums)
-        while i< (length):
-            if nums[i] == 0:
-                nums.pop(i)
-                nums.append(0)
-                length -=1
-            else:
-                i +=1
-        
+        l, r= 0,1
+        while r<len(nums):
+            if nums[l] ==0: 
+                if nums[r] != 0 :
+                    nums[l], nums[r] = nums[r],nums[l]
+                    l +=1
+            else: l += 1
+            r +=1
