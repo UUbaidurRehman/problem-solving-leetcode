193ee822a3f37b1a5def5523e06884e2a3cc5f84
UBAID-Urrehman0022 <119315741+UBAID-Urrehman0022@users.noreply.github.com>
2024-02-10 23:17:16 +0500

    Time: 33 ms (79.24%), Space: 16.5 MB (70.95%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -6,15 +6,14 @@ class Solution:
         if len1 < len2:
             for i in range(len1):
                 merge = merge + word1[i] + word2[i]
-            # merge = merge + word2[i:]
-            for i  in range (index,len2):
-                merge  = merge  + word2[i]
+            merge = merge + word2[i+1:]
+            # for i  in range (index,len2):
+            #     merge  = merge  + word2[i]
         elif len1 > len2:
             for i in range(len2):
                 merge = merge + word1[i] + word2[i]
-            # merge = merge + word2[i:]
-            for i  in range (index, len1):
-                merge  = merge  + word1[i]
+            merge = merge + word1[i+1:]
+            
         else:
             for i in range(len2):
                merge = merge + word1[i] + word2[i]
