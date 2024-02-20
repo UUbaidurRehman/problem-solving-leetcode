2b598571623eee2de1b5c2f7e2cf303dfd9854ec
UBAID-Urrehman0022 <119315741+UBAID-Urrehman0022@users.noreply.github.com>
2024-02-20 21:20:16 +0500

    Time: 98 ms (27.36%), Space: 22 MB (41.82%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -11,6 +11,6 @@ class Solution:
         res = []
         for i in range(len(freq)-1,-1 ,-1):
             for n in freq[i]:
-                res.append(j)
+                res.append(n)
                 if len(res) == k :
                     return res
