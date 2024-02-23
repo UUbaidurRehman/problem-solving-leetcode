53dd19387856fcc631beb5466bdd275816d837fd
UBAID-Urrehman0022 <119315741+UBAID-Urrehman0022@users.noreply.github.com>
2024-02-23 17:30:29 +0500

    Time: 40 ms (40.71%), Space: 16.7 MB (60.43%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.py
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.py
@@ -3,6 +3,13 @@ class Solution:
         count = 0
         for i in range(len(s)-2):
             window = s[i:i+3]
-            if len(set(window)) == len(window):
-                count += 1
+            isDistinct = True
+            seen = set()
+            for char in window:
+                if char in seen:
+                    isDistinct = False
+                    break
+                seen.add(char)
+            if isDistinct:
+                count +=1
         return count
