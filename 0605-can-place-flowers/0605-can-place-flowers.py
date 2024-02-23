a24876964f45b631396a69e05007783124c31dbf
UBAID-Urrehman0022 <119315741+UBAID-Urrehman0022@users.noreply.github.com>
2024-02-23 23:15:16 +0500

    Time: 139 ms (31.87%), Space: 16.9 MB (64.12%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -1,18 +1,21 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        
-        count, prev = 0, 0
-
-        for cur in flowerbed:
-            if cur == 1:
-                if prev == 1: 
-                    count -= 1
-                prev = 1
-            else:
-                if prev == 1: 
-                    prev = 0 
-                else:
+        count = 0
+        size = len(flowerbed)
+        for i in range(size):
+            if flowerbed[i] == 0:
+                if i ==0 or flowerbed[i-1] == 0: 
+                    prev = True
+                else: 
+                    prev = False
+                if i == size-1 or flowerbed[i+1] == 0: 
+                    nextt = True 
+                else: 
+                    nextt = False
+                if prev and nextt:
                     count += 1
-                    prev = 1 
-            
-        return count >= n
+                    flowerbed[i] = 1
+            if count >= n: return True
+        return False
+
+
