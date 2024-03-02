ea3296b24aedc957c762c66038f670a298581aeb
UBAID-Urrehman0022 <119315741+UBAID-Urrehman0022@users.noreply.github.com>
2024-03-02 23:00:52 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -2,12 +2,13 @@ class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         l, r = 0, len(s1)-1
         a = [0]*26
-        for i in (s1):
-            a[ord(i) - ord('a')  ] += 1
-        
         b = [0]*26
-        for i in (s2[l:r+1]):
-                b[ord(i) - ord('a') ] += 1
+        for i in range(len(s1)):
+            a[ord(s1[i]) - ord('a')  ] += 1
+            b[ord(s2[i]) - ord('a') ] += 1
+        
+        # for i in (s2[l:r+1]):
+        #         b[ord(i) - ord('a') ] += 1
         while r< len(s2):
             if a == b:
                 return True
