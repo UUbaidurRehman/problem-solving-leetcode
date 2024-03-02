e4bc4e90631b5a98251de7c309e6978df53782b1
UBAID-Urrehman0022 <119315741+UBAID-Urrehman0022@users.noreply.github.com>
2024-03-02 22:46:34 +0500

    Time: 111 ms (34.35%), Space: 16.6 MB (85.83%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -3,18 +3,18 @@ class Solution:
         l, r = 0, len(s1)-1
         a = [0]*26
         for i in (s1):
-            a[ord(i) - ord('a')  ] = 1
+            a[ord(i) - ord('a')  ] += 1
         
         b = [0]*26
         for i in (s2[l:r+1]):
-                b[ord(i) - ord('a') ] = 1
+                b[ord(i) - ord('a') ] += 1
         while r< len(s2):
             c = sum(1 for j in range(26) if a[j] == b[j])
             if c == 26:
                 return True
-            b[ord(s2[l]) - ord('a')] = 0
+            b[ord(s2[l]) - ord('a')] -= 1
             l += 1
             r += 1
             if r<len(s2):
-                b[ord(s2[r]) - ord('a')] = 1
+                b[ord(s2[r]) - ord('a')] += 1
         return False
