c8766b65fe47748bdd74f1600db40a508063e236
UBAID-Urrehman0022 <119315741+UBAID-Urrehman0022@users.noreply.github.com>
2024-03-03 00:32:16 +0500

    Time: 46 ms (47.38%), Space: 16.8 MB (76.75%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.py
+++ 0682-baseball-game/0682-baseball-game.py
@@ -3,15 +3,14 @@ class Solution:
         stack = []
         total = 0
         for i in operations:
-            if i == "C":
+            if i == "C": 
                 stack.pop()
             elif i == "D":
                 stack.append(2 * stack[-1])
             elif i == "+":
-                # if len(stack) >= 2:
                 stack.append(stack[-1] + stack[-2])
             else:
                 stack.append(int(i))
-        for i in stack :
+        for i in stack:
             total += i
         return total
