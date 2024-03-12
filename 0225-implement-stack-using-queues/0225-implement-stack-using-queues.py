1194bb6f2579af2a562fb4ed019f5897830a4c4a
UBAID-Urrehman0022 <119315741+UBAID-Urrehman0022@users.noreply.github.com>
2024-03-12 12:46:03 +0500

    Time: 36 ms (49.44%), Space: 16.6 MB (76.63%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
@@ -9,8 +9,11 @@ class MyStack:
             self.push(self.q.popleft())
         return self.q.popleft()
     def top(self) -> int:
-        return self.q[-1]
-
+        for i in range(len(self.q)-1):
+            self.push(self.q.popleft())
+        res = self.q[0]
+        self.push(self.q.popleft())
+        return res
     def empty(self) -> bool:
         return len(self.q) == 0
 
