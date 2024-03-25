c183a10771c76fe747715dec3560d729997e121b
UBAID-Urrehman0022 <119315741+UBAID-Urrehman0022@users.noreply.github.com>
2024-03-25 22:54:37 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
@@ -1,8 +1,7 @@
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
         l ,r = 0 , len(nums)-1
-        if r== 0:
-            return nums[r]
+        
         while l < r :
             mid = l + (r-l)//2
             if (mid % 2 == 0 and nums[mid]==nums[mid+1]) or \
@@ -13,36 +12,6 @@ class Solution:
                     r = mid
             else:
                 return nums[mid]
-        return nums[l]
-
-        
-
-
-
-        
-
-
-
-
-
-        # l,r = 0, len(nums)
-        # if len(nums) <=1:
-        #     return nums[r-1]
-        # while (l < r):
-        #     mid = l + (r-l)//2
-        #     if mid % 2 == 0:
-        #         if nums[mid] == nums[mid-1]:
-        #             r = mid
-        #         elif nums[mid] == nums[mid+1]:
-        #             l = mid+1
-        #         else:
-        #             return nums[mid]
-
-        #     else:
-        #         if nums[mid] == nums[mid-1]:
-        #             l = mid+1
-        #         elif nums[mid] == nums[mid+1]:
-        #             r = mid
-        #         else:
-        #             return nums[mid]
-        # return mid
+        if r== 0:
+            return nums[r]
+        # return nums[l]  
